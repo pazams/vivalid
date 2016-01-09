0bfa5f35d5ee20549cd512228ef85a85e8137988
pazams <maor@pazams.com>
2016-01-09 19:37:49 +0200

    added "hidden" to valid input types

--- dist/vivalid-bundle.js
+++ dist/vivalid-bundle.js
@@ -1,8 +1,8 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.vivalid = f()}})(function(){var define,module,exports;return (require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.vivalid = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 module.exports = {
     VERSION: '0.1.0',
     DEBUG: false,
-    validInputTagNames: ['input', 'textarea', 'select'],
+    validInputTagNames: ['input', 'textarea', 'select', 'hidden'],
     keyStrokedInputTypes: ['text', 'email', 'password', 'search'],
     ERROR: {
         mandatorySuccessFailure: 'passing callbacks for onValidationSuccess and onValidationFailure is mandatory'
@@ -912,5 +912,5 @@ module.exports = {
     _ERROR: constants.ERROR
 };
 
-},{"./constants":1,"./html-interface":3,"./input":5,"./input-group":4,"./state-enum":6,"./validator-repo":8}]},{},["vivalid"]))("vivalid")
+},{"./constants":1,"./html-interface":3,"./input":5,"./input-group":4,"./state-enum":6,"./validator-repo":8}]},{},["vivalid"])("vivalid")
 });
